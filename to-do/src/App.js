f8d75b83fb4152c8a86f7ed149276a68597885cc
Terry Carpenter <terry.carpenter@outlook.com>
2022-11-09 17:39:47 -0600

    first commit

---
+++ to-do/src/App.js
@@ -0,0 +1,49 @@
+import React, { useState } from "react";
+
+function App() {
+  const [list, setList] = useState([]);
+  const [input, setInput] = useState("");
+
+  const addTodo = (todo) => {
+    const newTodo = {
+    id: Math.random(),
+    todo: todo,
+    };
+  };
+
+  // add to the todo list
+  setList([...list, newTodo]);
+
+  //clear input box
+  setInput("");
+
+  const deleteTodo = (id) => {
+    const newList = list.filter((todo) => todo.id !== id);
+
+    setList(newList);
+  };
+
+
+
+return (
+  <div>
+    <h1>To Do List </h1>
+    <input
+      type="text"
+      value={input}
+      onChange={(e) => setInput(e.target.value)}
+    />
+    <button onClick={() => addTodo(input)}>Add</button>
+    <ul>
+      {lidt.map((todo) => (
+        <li key={todo.id}>
+          {todo.todo}
+          <button onClick= {() => deleteTodo(todo.id)}>&times;</button>
+        </li>
+      ))}
+      </ul>
+  </div>
+);
+}
+
+export default App
